61931fdc30712f3b78ee344f14e74e0abd8a2012
Fernando Espericueta <fernandostark18@gmail.com>
2023-12-04 23:05:37 -0600

    Child to parent Component Communication (Bottom-up)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import "./ExpenseForm.css";
 import { useState } from "react";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [userInput, setUserInput] = useState({
     enteredTitle: "",
     enteredAmount: "",
@@ -28,7 +28,7 @@ const ExpenseForm = () => {
       date: new Date(userInput.enteredDate),
     };
 
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setUserInput({
       ...userInput,
       enteredTitle: "",
